5a9e561c2f5168875e56f26cf3a48b1a617f95ec
prashant mishra <mishra.pm443@gmail.com>
2026-06-12 12:28:17 +0530

    updated

---
+++ app/api/test-firebase/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { adminDb } from "@/firebase/firebaseAdmin";
+
+export async function GET() {
+  try {
+    await adminDb
+      .collection("test")
+      .doc("connection")
+      .set({
+        message: "Firebase Admin Working",
+        createdAt: new Date(),
+      });
+
+    return NextResponse.json({
+      success: true,
+      message: "Firebase Admin Connected",
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
